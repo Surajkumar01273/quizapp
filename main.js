04c7182e9b35d37a999b32a179f7ad01e5661c4c
Suraj kumar <surajshk1995@gmail.com>
2024-10-11 15:00:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const questions = [
+    {
+        question: "What is standard for HTML?",
+        answer: [
+           { text: "Hyper Text Markup Language", correct: true},
+           {text: "ssd", correct: false},
+           {text: "ff", correct: false},
+           {text: "dfd", correct: false}
+        ]
+    },
+  
+    {
+        question: "What is standard for HTML?",
+        answer: [
+           {text: "Hyper Text Markup Language", correct: true},
+           {text: "ssd", correct: false},
+           {text: "ff", correct: false},
+           {text: "dfd", correct: false}
+        ]
+    },
+  
+    {
+        question: "What is standard for HTML?",
+        answer: [
+           { text: "Hyper Text Markup Language", correct: true},
+           {text: "ssd", correct: false},
+           {text: "ff", correct: false},
+           {text: "dfd", correct: false}
+        ]
+    },
+  
+]
+
+
+const inputquestion = document.getElementById('question')
+const answerOption = document.getElementById('answer-button')
+
+console.log(questions.question);
+
+
+
